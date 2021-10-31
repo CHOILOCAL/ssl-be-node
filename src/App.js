208a41810606178971a91d2a03eaa314c15f947a
CHOILOCAL <giants1217@naver.com>
2021-11-01 08:09:45 +0900

    Fargate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <form
+                action="/api/v1/upload"
+                enctype="multipart/form-data"
+                method="post"
+            >
+                <input type="file" name="img_files[]" accept="image/*" />
+                <button type="submit">Submit</button>
+            </form>
+        </>
+    );
 }
 
 export default App;
---
+++ src/routes/post.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+
+const { Post, Hashtag } = require('../models');
+const { isLoggedIn } = require('./middlewares');
+
+const router = express.Router();
+
+try {
+  fs.readdirSync('uploads');
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
+  fs.mkdirSync('uploads');
+}
